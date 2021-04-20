3a931086f9b5ec27d265d53f2cb282d02f128792
SolomonKitonyi <kitonyisolomon@gmail.com>
2021-04-21 00:45:47 +0300

    add link

--- src/user/components/UserItem.js
+++ src/user/components/UserItem.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import "./UserItem.css";
 import Avatar from "../../shared/UIElements/Avatar";
@@ -7,16 +8,18 @@ const UserItem = (props) => {
 	return (
 		<li className="user-item">
 			<div className="user-item__content">
-				<div className="user-item__image">
-					<Avatar alt={props.name} />
-				</div>
-				<div className="user-item__info">
-					<h1>{props.name}</h1>
-					<h3>
-						{props.placeCount}
-						{props.placeCount === 1 ? "place" : "places"}
-					</h3>
-				</div>
+				<Link to={`/${props.id}/places`}>
+					<div className="user-item__image">
+						<Avatar alt={props.name} />
+					</div>
+					<div className="user-item__info">
+						<h1>{props.name}</h1>
+						<h3>
+							{props.placeCount}
+							{props.placeCount === 1 ? "place" : "places"}
+						</h3>
+					</div>
+				</Link>
 			</div>
 		</li>
 	);
