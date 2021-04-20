042a73aeef25a416a29e358497fe3e01d54f422c
SolomonKitonyi <kitonyisolomon@gmail.com>
2021-04-20 17:09:11 +0300

    add switch

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
+import {
+	BrowserRouter as Router,
+	Route,
+	Redirect,
+	Switch,
+} from "react-router-dom";
 
 import Users from "./user/pages/Users";
 import NewPlace from "./places/pages/NewPlace";
 
 function App() {
 	return (
-		<h1>
-			<Router>
-				<div>
-					<Route path="/" exact component={Users} />
-					<Route path="/places/new" component={NewPlace} />
-				</div>
+		<Router>
+			<Switch>
+				<Route path="/" exact component={Users} />
+				<Route path="/places/new" component={NewPlace} />
 				<Redirect to="/" />
-			</Router>
-		</h1>
+			</Switch>
+		</Router>
 	);
 }
 
