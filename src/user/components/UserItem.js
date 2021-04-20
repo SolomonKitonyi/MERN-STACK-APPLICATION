93771be08bd9e4935aac20cb27512b9ad5b06c36
SolomonKitonyi <kitonyisolomon@gmail.com>
2021-04-20 17:33:48 +0300

    add user item and users list components

---
+++ src/user/components/UserItem.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./UserItem.css";
+
+const UserItem = (props) => {
+	return <div>Item</div>;
+};
+
+export default UserItem;
---
+++ src/user/components/UsersList.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./UsersList.css";
+
+const UsersList = (props) => {
+	return <div>list</div>;
+};
+
+export default UsersList;
