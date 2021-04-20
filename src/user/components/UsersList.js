5b1c62bba5f4bce65810ececb36a8c8bbfc1a035
SolomonKitonyi <kitonyisolomon@gmail.com>
2021-04-20 17:43:57 +0300

    add changes

--- src/user/components/UsersList.js
+++ src/user/components/UsersList.js
@@ -1,9 +1,32 @@
 import React from "react";
 
 import "./UsersList.css";
+import UserItem from "./UserItem";
 
 const UsersList = (props) => {
-	return <div>list</div>;
+	if (props.items == 0) {
+		return (
+			<div>
+				<h1>No Users Found</h1>
+			</div>
+		);
+	}
+
+	return (
+		<ul>
+			{props.items.map(() => {
+				return (
+					<UserItem
+						key={user.id}
+						id={user.id}
+						image={user.image}
+						name={user.name}
+						placeCount={user.places}
+					/>
+				);
+			})}
+		</ul>
+	);
 };
 
 export default UsersList;
