edc857a88ec1f7a412fa3eb8a24c16a376fa4fd3
SolomonKitonyi <kitonyisolomon@gmail.com>
2021-04-20 17:52:26 +0300

    show users list

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,7 +1,17 @@
 import React from "react";
 
+import UsersList from "../components/UsersList";
+
 const Users = () => {
-	return <h1>Users works !</h1>;
+	const USERS = [
+		{
+			id: "u1",
+			name: "Solomon",
+			image: "https://unsplash.com/photos/6VPEOdpFNAs",
+			places: 3,
+		},
+	];
+	return <UsersList items={USERS} />;
 };
 
 export default Users;
